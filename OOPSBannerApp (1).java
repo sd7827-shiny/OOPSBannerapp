417326f84b604c7c9a9d99dd100a143fb626d2cc
sd7827-shiny <sd7827@srmist.edu.in>
2026-03-02 08:55:28 +0530

    Initial commit

---
+++ OOPSBannerApp (1).java
@@ -0,0 +1,13 @@
+/**
+ * UC1 - Print the literal text "OOPS" to the console.
+ * This program runs directly inside VS Code.
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Print OOPS
+        System.out.println("OOPS");
+
+    }
+}
