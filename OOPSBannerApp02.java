f371bd2c4ea23b3ac1ded7a9c89c1fc372d0006c
sd7827-shiny <sd7827@srmist.edu.in>
2026-03-02 09:03:02 +0530

    Describe what you changed

---
+++ OOPSBannerApp02.java
@@ -0,0 +1,19 @@
+/**
+ * UC2 - Display "OOPS" as a large banner using '*' characters.
+ * Runs directly inside VS Code.
+ */
+public class OOPSBannerApp02 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****   ******   *****  ");
+        System.out.println("*     *  *     *  *     * *     * ");
+        System.out.println("*     *  *     *  *     * *       ");
+        System.out.println("*     *  *     *  ******   *****  ");
+        System.out.println("*     *  *     *  *             * ");
+        System.out.println("*     *  *     *  *       *     * ");
+        System.out.println(" *****    *****   *        *****  ");
+
+    }
+}
+
